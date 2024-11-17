9e98688e979af622189f275d55029edebed34a0e
Whyaftab01 <aftablaghari502@gmail.com>
2024-11-17 19:05:03 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "RCD-MD&H1c1gk5J",
+  SESSION_ID: process.env.SESSION_ID || "RCD-MD&tp6VcQ0f",
   triggerWords: [
     'send', 'statusdown', 'take', 'sent', 'giv', 'gib', 'upload',
     'send me', 'sent me', 'znt', 'snt', 'ayak', 'do', 'mee', 'autoread'
@@ -25,9 +25,9 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "✪⏤RCD",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94781575085",
+  MODE: process.env.MODE || "private",
+  OWNER_NAME: process.env.OWNER_NAME || "whyaftab",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "923103278551",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
